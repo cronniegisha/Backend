0fd0919bfa8b81889696ae3f7d633157c1fc6c3e
Cronnie Owomugisha <owomugisha.cronnie@students.mak.ac.ug>
2025-05-09 21:14:22 +0300

    Add whitenoise

--- config/settings.py
+++ config/settings.py
@@ -91,6 +91,7 @@ AUTH_USER_MODEL = 'matching.User'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
